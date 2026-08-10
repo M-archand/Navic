1e21563564805e7783aca3d2a17e4b8396d54e5a
paige <paigely@tuta.io>
2026-08-10 01:03:38 -0500

    chore: bump versions

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -20,8 +20,8 @@ extensions.configure<ApplicationExtension> {
 		applicationId = "paige.navic"
 		minSdk = libs.versions.android.minSdk.get().toInt()
 		targetSdk = libs.versions.android.targetSdk.get().toInt()
-		versionCode = 44
-		versionName = "v1.0.0-alpha44"
+		versionCode = 45
+		versionName = "v1.0.0-alpha45"
 
 		ndk {
 			abiFilters.addAll(listOf("arm64-v8a", "armeabi-v7a"))
